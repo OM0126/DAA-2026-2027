d54bce50de49264add3aaac2e27423ab369357b1
om yengantiwar <omgithub01@gmail.com>
2026-07-27 17:26:22 +0530

    Update Binary_Search.py for dynamic input
    Refactored binary search to accept dynamic input for the list of numbers.

--- practical 2/Binary_Search.py
+++ practical 2/Binary_Search.py
@@ -1,11 +1,15 @@
-# Binary Search in Python
+n = int(input("Enter the number of elements: "))
 
-numbers = [10, 20, 30, 40, 50, 60, 70]
+numbers = []
+
+print("Enter the elements:")
+for i in range(n):
+    numbers.append(int(input()))
 
 target = int(input("Enter the number to search: "))
 
 low = 0
-high = len(numbers) - 1
+high = n - 1
 
 while low <= high:
     mid = (low + high) // 2
@@ -13,16 +17,13 @@ while low <= high:
     if numbers[mid] == target:
         print("Number found at index", mid)
         break
-
     elif numbers[mid] < target:
         low = mid + 1
-
     else:
         high = mid - 1
 
 if low > high:
     print("Number not found")
-
 
 
 # Enter the number of elements: 7
